9966a3c2c3d3866acd567a6d5abd5e9a27a5dc39
masha14013 <masha14013@rambler.ru>
2022-10-02 02:26:38 +0300

    added file fighter.js

---
+++ fighter.js
@@ -0,0 +1,8 @@
+var quote = function(fighter) {
+  fighter = fighter.toUpperCase();
+  if (fighter == "GEORGE SAINT PIERRE") {
+    return "I am not impressed by your performance.";
+  } else if (fighter == "CONOR MCGREGOR") {
+    return "I'd like to take this chance to apologize.. To absolutely NOBODY!";
+  }
+};
