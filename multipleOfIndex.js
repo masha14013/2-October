81fa4ec3883f873c5b4e2ac282ecdc55322850f5
masha14013 <masha14013@rambler.ru>
2022-10-02 16:25:15 +0300

    added file multipleOfIndex.js

---
+++ multipleOfIndex.js
@@ -0,0 +1,11 @@
+function multipleOfIndex(array) {
+  let newArr = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % i == 0) {
+      newArr.push(array[i]);
+    } else {
+      continue;
+    }
+  }
+  return newArr;
+}
