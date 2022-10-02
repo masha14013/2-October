363deebe4a59b28d218b360bb1cccac46377dfed
masha14013 <masha14013@rambler.ru>
2022-10-02 13:55:21 +0300

    added file convertHTML.js

---
+++ convertHTML.js
@@ -0,0 +1,10 @@
+function convertHTML(str) {
+  str = str.replace(/&/gi, "&amp;");
+  str = str.replace(/</gi, "&lt;");
+  str = str.replace(/>/gi, "&gt;");
+  str = str.replace(/\"/gi, "&quot;");
+  str = str.replace(/\'/gi, "&apos;");
+  return str;
+}
+
+convertHTML("Dolce & Gabbana");
